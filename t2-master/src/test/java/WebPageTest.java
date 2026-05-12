58abdcc42b0040ca9c99d073203eba336a21b686
chxyx <rupali111247@gmail.com>
2026-05-12 10:16:23 +0530

    update

---
+++ t2-master/src/test/java/WebPageTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class WebPageTest {
+
+    @Test
+    public void sampleTest() {
+
+        System.out.println("Test Executed Successfully");
+
+        assertTrue(true);
+    }
+}
